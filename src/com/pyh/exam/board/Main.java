fbd7d7a12cde7c8502c6dfe8e7a117e1651c6877
yuuuuuni <yuuuuuni930302@gmail.com>
2021-11-16 22:38:22 +0900

    exit가 입력 될 때까지 명령어 계속 입력받기

--- src/com/pyh/exam/board/Main.java
+++ src/com/pyh/exam/board/Main.java
@@ -7,11 +7,25 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    String str;
+
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.printf("명령) ");
-    String str = sc.nextLine();
-    System.out.println("입력된 명령어 : " + str);
+
+    while(true) {
+      System.out.printf("명령) ");
+      str = sc.nextLine();
+
+      if(str.equals("exit")) {
+        break;
+      }
+
+      System.out.println("입력된 명령어 : " + str);
+
+    }
+
+
+
     System.out.println("== 프로그램 종료 ==");
 
     sc.close();
