55b4814a3f5be500b8083c1706cefcbcfdfefcba
yuuuuuni <yuuuuuni930302@gmail.com>
2021-11-16 22:01:06 +0900

    명령어 입력

--- src/com/pyh/exam/board/Main.java
+++ src/com/pyh/exam/board/Main.java
@@ -1,13 +1,19 @@
 package com.pyh.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.println("명령)");
+    System.out.printf("명령) ");
+    String str = sc.nextLine();
+    System.out.println("입력된 명령어 : " + str);
     System.out.println("== 프로그램 종료 ==");
 
+    sc.close();
   }
 }
