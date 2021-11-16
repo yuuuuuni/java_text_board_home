98788d19e25bee725f87920069d8213e3ce9ce1c
yuuuuuni <yuuuuuni930302@gmail.com>
2021-11-16 21:26:04 +0900

    안녕 메시지 지움

--- src/com/pyh/exam/board/Main.java
+++ src/com/pyh/exam/board/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
   public static void main(String[] args) {
 
-    System.out.println("안녕");
-
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
     System.out.println("명령)");
