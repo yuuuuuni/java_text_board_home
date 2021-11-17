e5a92e422fd0dbd428d4df613987363d14708a58
yuuuuuni <yuuuuuni930302@gmail.com>
2021-11-17 17:01:50 +0900

    입력받은 게시물 정보를 Article 객체에 저장

--- src/com/pyh/exam/board/Main.java
+++ src/com/pyh/exam/board/Main.java
@@ -8,40 +8,55 @@ public class Main {
     Scanner sc = new Scanner(System.in);
 
 
-
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
 
     int articlesLastId = 0;  // 초장기에는 아직 게시물이 없을 때이므로 0으로 간다
 
-    while(true) {
+    while (true) {
       System.out.printf("명령) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("exit")) {
+      if (cmd.equals("exit")) {
         break;
-      }
-      else if(cmd.equals("/usr/article/write")) {
-          System.out.println("- 게시물 등록 -");
-          System.out.printf("제목 : ");
-          String title = sc.nextLine();
-          System.out.printf("내용 : ");
-          String body = sc.nextLine();
-          int id = articlesLastId + 1;
-        articlesLastId ++;
-          System.out.println(id + "번 게시물이 등록되었습니다.");
+      } else if (cmd.equals("/usr/article/write")) {
+        System.out.println("- 게시물 등록 -");
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+        int id = articlesLastId + 1;
+        articlesLastId++;
 
-      }
-      else {
+        Article article = new Article();     // article 객체 생성
+        article.id = id;
+        article.title = title;
+        article.body = body;
+        System.out.println("생성된 게시물 객체" + article);
+
+        System.out.println(article.id + "번 게시물이 등록되었습니다.");
+
+
+
+
+
+      } else {
         System.out.println("입력된 명령어 : " + cmd);
       }
 
     }
 
 
-
     System.out.println("== 프로그램 종료 ==");
 
     sc.close();
   }
+}
+
+
+
+class Article {
+  int id;
+  String title;
+  String body;
 }
