32011610207c71960f45dd26992ef2acb8761cb7
yuuuuuni <yuuuuuni930302@gmail.com>
2021-11-17 15:54:57 +0900

    새 게시물 입력받기 기능 구현 시작

--- src/com/pyh/exam/board/Main.java
+++ src/com/pyh/exam/board/Main.java
@@ -7,18 +7,30 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    String str;
+
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
 
     while(true) {
       System.out.printf("명령) ");
-      str = sc.nextLine();
-      System.out.println("입력된 명령어 : " + str);
+      String cmd = sc.nextLine();
 
-      if(str.equals("exit")) {
+      if(cmd.equals("exit")) {
         break;
+      }
+      else if(cmd.equals("/usr/article/write")) {
+          System.out.println("- 게시물 등록 -");
+          System.out.printf("제목 : ");
+          String title = sc.nextLine();
+          System.out.printf("내용 : ");
+          String body = sc.nextLine();
+          int id = 1;
+          System.out.println(id + "번 게시물이 등록되었습니다.");
+
+      }
+      else {
+        System.out.println("입력된 명령어 : " + cmd);
       }
 
     }
